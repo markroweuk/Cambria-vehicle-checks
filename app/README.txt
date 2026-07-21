ad79e2b41aa46799e51b5eea085648c0f04bfab1
markroweuk <markroweuk@icloud.com>
2026-07-21 17:40:14 +0100

    Add files via upload

---
+++ app/README.txt
@@ -0,0 +1,6 @@
+Place the complete web application files here.
+
+Production release:
+Cambria Vehicle Check v3.2.0
+
+This package is the single source of truth.
---
+++ config.js
@@ -0,0 +1,2 @@
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxZQ-9YmIszijdQ5TYSeXX7uZgCGeiTk3ccEOIa2a5-vwcaX1LEbo_X4x_VgxzooJc5/exec";
+const APP_VERSION = "3.2.0";
